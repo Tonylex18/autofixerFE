fcdb4dd90990db22dc527088832c0063664fc66a
Tonylex18 <anthonyagada2000@gmail.com>
2023-11-03 09:21:58 +0100

    updated file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js,css}"],
+  content: ["./src/app/**/*.{html,js,css}"],
   theme: {
     extend: {},
   },
